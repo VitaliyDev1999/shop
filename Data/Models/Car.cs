4f9a90c3d3936b77ced25db7279f5fb05301c86c
VitaliyPC <deinega.vit1@gmail.com>
2019-07-19 16:11:23 +0300

    add 2 models

---
+++ Data/Models/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Data.Models
+{
+    public class Car
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public string shortDesc { get; set; }
+        public string longDesc { get; set; }
+        public string img { get; set; }
+        public ushort price { get; set; }
+        public int available { get; set; }
+        public int categoryId { get; set; }
+        public virtual Categoty Categoty { get; set; }
+
+    }
+}
