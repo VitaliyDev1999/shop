971a9f09df72a8efb0b3f2dffeb9c3a263c9d0dc
VitaliyPC <deinega.vit1@gmail.com>
2019-07-25 22:20:19 +0300

    add entity framework and configure db connection

--- Startup.cs
+++ Startup.cs
@@ -5,7 +5,10 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Shop.Data;
 using Shop.Data.Interfaces;
 using Shop.Data.Mocks;
 
@@ -13,10 +16,18 @@ namespace Shop
 {
     public class Startup
     {
+
+        private IConfigurationRoot _confString;
+
+        public Startup(IHostingEnvironment hostEnv)
+        {
+            _confString = new ConfigurationBuilder().SetBasePath(hostEnv.ContentRootPath).AddJsonFile("bdsettings.json").Build();
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDBContent>(options => options.UseSqlServer(_confString.GetConnectionString("DefaultConnection")));
             services.AddMvc();
             services.AddTransient < IAllCars,MockCars>();
             services.AddTransient<ICarsCategory, MockCategory>();
